c748e87a7e49639b0181947bf824fded24f55361
Denis Zadorozhko <zdenis1990@gmail.com>
2021-10-21 09:58:49 +0300

    init commit

---
+++ src/router/index.js
@@ -0,0 +1,35 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import EventList from '../views/EventList.vue';
+import EventDetails from '../views/EventDetails.vue';
+import About from '../views/About.vue';
+
+const routes = [
+  {
+    path: '/',
+    name: 'EventList',
+    component: EventList
+  },
+  {
+    path: '/event/:id',
+    name: 'EventDetails',
+    component: EventDetails,
+    props: true
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    // component: () =>
+    // import(/* webpackChunkName: "about" */ "../views/About.vue")
+  }
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+});
+
+export default router;
---
+++ src/views/EventList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EventCard from '@/components/EventCard.vue';
+import EventService from '@/services/EventService.js';
+
+export default {
+  name: 'Home',
+  components: {
+    EventCard
+  },
+  data() {
+    return {
+      events: null
+    };
+  },
+  created() {
+    EventService.getEvents()
+      .then(response => (this.events = response.data))
+      .catch(err => console.log(err));
+  }
+};
+</script>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
